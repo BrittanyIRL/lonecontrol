a90603792fc7fd59960fc91855e742aebb511d6f
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-07-26 13:08:39 -0700

    swap out hard coded text for graphql content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 // import PropTypes from "prop-types"
 import React, { useState } from "react"
+import { graphql } from "gatsby"
 import styled from "styled-components"
 
 import Layout from "../components/layout"
@@ -90,128 +91,87 @@ const AboutSection = styled.div`
   transition: opacity 1000ms, visibility 1000ms;
 `
 
-const About = () => {
+const SEO_ABOUT_KEYWORDS = [
+  `phoenix`,
+  `punk`,
+  `music`,
+  `chris`,
+  `gerrit`,
+  `desert drip`,
+  `new music`,
+  `unnecessary voice`,
+  `lone control`,
+  `loan control`,
+  `arizona`,
+  `rock`,
+  `indie`,
+  `band`,
+]
+
+const About = ({ data }) => {
+  const {
+    title,
+    leadImage,
+    leadImageAlt,
+    etherealLead,
+    etherealContent,
+    tangibleLead,
+    tangibleContent,
+  } = data.file.childMarkdownRemark.frontmatter
+
   const [openEtherealSection, toggleOpenEtherealSection] = useState(true)
   return (
     <Layout>
-      <SEO
-        title="About"
-        keywords={[
-          `phoenix`,
-          `punk`,
-          `music`,
-          `chris`,
-          `gerrit`,
-          `desert drip`,
-          `new music`,
-          `unnecessary voice`,
-          `lone control`,
-          `loan control`,
-          `arizona`,
-          `rock`,
-          `indie`,
-          `band`,
-        ]}
-      />
-      <PageHeading>About</PageHeading>
-      <AboutImage
-        src={bandImage}
-        alt="Gerrit Feenstra and Chris Trian, the two members of Lone Control."
-      />
+      <SEO title="About" keywords={SEO_ABOUT_KEYWORDS} />
+      <PageHeading>{title}</PageHeading>
+      <AboutImage src={leadImage} alt={leadImageAlt} />
       <AccordionControls>
         <AccordionButton
           isExpanded={openEtherealSection}
           disabled={openEtherealSection}
           onClick={() => toggleOpenEtherealSection(!openEtherealSection)}
         >
-          <SectionHeading>Ethereal version</SectionHeading>
+          <SectionHeading>{etherealLead}</SectionHeading>
         </AccordionButton>
         <AccordionButton
           isExpanded={!openEtherealSection}
           disabled={!openEtherealSection}
           onClick={() => toggleOpenEtherealSection(!openEtherealSection)}
         >
-          <SectionHeading>Tangible version</SectionHeading>
+          <SectionHeading>{tangibleLead}</SectionHeading>
         </AccordionButton>
       </AccordionControls>
       <AccordionContainer>
-        <AboutSection isExpanded={openEtherealSection}>
-          <p>Lone Control is a blip. Shrill. Loud. And now.</p>
-
-          <p>
-            You can live your life without so many things, so many experiences,
-            purchased or otherwise.
-          </p>
-
-          <p>
-            You can live your life without hearing Lone Control or the Rolling
-            Stones.
-          </p>
-
-          <p>
-            Or, you can live inclusively. Inclusive of joy, of pain, and of the
-            noise between, wherein Lone Control may be your guide.
-          </p>
-
-          <p>
-            Lone Control occupy the space of the now, a disposable place of
-            anxiety and aggregation. The future holds only the next moment, or a
-            thousand miles of blank space. Dealer’s choice, curated for
-            convenience.
-          </p>
-
-          <p>
-            Lone Control question the means of the why. The push pull incentive
-            of aging and irrelevance. One inconsequential notion in a long array
-            of impotence. And yet, we exist. We breathe. We sweat. We bleed and
-            fight and beg for meaning. The silence is an invitation.
-          </p>
-
-          <p>
-            Lone Control’s debut is a flash in the pan, arresting, burning
-            bright while waning focus may last. Four hints at a larger question,
-            too large to answer now. That moment will come in time. For now,
-            there is only this. Here. There. Gone.
-          </p>
-        </AboutSection>
-        <AboutSection isExpanded={!openEtherealSection}>
-          <p>Lone Control is a blip. Shrill. Loud. And now.</p>
-
-          <p>
-            Lone Control is born from common desire. Desire to break free of the
-            predetermination of the daily millstone grind. This is not a
-            hardened desire, but one that continues to bubble and burst with
-            time. What Chris and Gerrit each bring to the creative circle rarely
-            overlaps. These are unfitting pieces, made to lay together
-            awkwardly, unrefined and unhinged.
-          </p>
-
-          <p>
-            Chris and Gerrit met at a downtown Phoenix bar. The former, from an
-            El Paso punk background, fresh to the city. The latter, a return
-            resident, after years apart making electronic music in Seattle. The
-            two hit it off immediately, finding common ground in Nine Inch
-            Nails, Brainiac, and the early works of Steve Albini.
-          </p>
-
-          <p>
-            Older than they once were but not yet beyond the pale, Lone Control
-            represents a knowing outburst – a cry, not for help or attention,
-            but to rally any who might find need. Their creations are crude and
-            abrasive by design. Rarely in this age of immediacy do they find
-            meaningful value in the overwrought.{" "}
-          </p>
-
-          <p>
-            Lone Control’s debut is a flash in the pan, arresting, burning
-            bright while waning focus may last. Four hints at a larger question,
-            too large to answer now. That moment will come in time. For now,
-            there is only this. Here. There. Gone.{" "}
-          </p>
-        </AboutSection>
+        <AboutSection
+          isExpanded={openEtherealSection}
+          dangerouslySetInnerHTML={{ __html: etherealContent }}
+        />
+        <AboutSection
+          isExpanded={!openEtherealSection}
+          dangerouslySetInnerHTML={{ __html: tangibleContent }}
+        />
       </AccordionContainer>
     </Layout>
   )
 }
 
 export default About
+
+export const query = graphql`
+  {
+    file(relativePath: { eq: "about.md" }) {
+      id
+      childMarkdownRemark {
+        frontmatter {
+          title
+          leadImage
+          leadImageAlt
+          etherealLead
+          etherealContent
+          tangibleLead
+          tangibleContent
+        }
+      }
+    }
+  }
+`
